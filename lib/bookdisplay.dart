fae37080f642ac631c9984e51051e82a13e2dffb
Akash kr <akashkr971@gmail.com>
2025-01-19 19:32:37 +0530

    Added page numbers so that when changed move to that page

--- lib/bookdisplay.dart
+++ lib/bookdisplay.dart
@@ -1,10 +1,7 @@
-// ignore_for_file: deprecated_member_use
-
-import 'dart:io';
+// ignore_for_file: deprecated_member_use, avoid_print
 
 import 'package:flutter/material.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
-import 'package:pdf_text/pdf_text.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 
 class BookDisplay extends StatefulWidget {
@@ -20,121 +17,101 @@ class _BookDisplayState extends State<BookDisplay> {
   int _totalPages = 0;
   int _currentPage = 0;
   bool isPlaying = false;
-  String _searchQuery = '';
-  final TextEditingController _searchController = TextEditingController();
-  bool _isSearching = false;
-
-  late PDFDoc _pdfDocument;
   final FlutterTts _flutterTts = FlutterTts();
+  final TextEditingController _pageController = TextEditingController();
+  PDFViewController? _pdfViewController;
 
   @override
   void initState() {
     super.initState();
-    _loadPdf();
-  }
-
-  Future<void> _loadPdf() async {
-    _pdfDocument = await PDFDoc.fromFile(File(widget.book[1]));
-    setState(() {
-      _totalPages = _pdfDocument.length;
-    });
-  }
-
-  void _searchText(String query) async {
-    setState(() {
-      _searchQuery = query;
-    });
-
-    if (_searchQuery.isNotEmpty) {
-      for (int page = 0; page < _totalPages; page++) {
-        String pageText = await _getPageText(page);
-        if (pageText.contains(_searchQuery)) {
-          setState(() {
-            _currentPage = page;
-          });
-          break;
-        }
-      }
-    }
-  }
-
-  Future<String> _getPageText(int pageIndex) async {
-    PDFPage page = _pdfDocument.pageAt(pageIndex);
-    String text = await page.text;
-    return text;
-  }
-
-  void _startTTS() async {
-    // Start reading from the highlighted text (currently from the search query text)
-    String pageText = await _getPageText(_currentPage);
-    String textToRead = _searchQuery.isEmpty ? pageText : _searchQuery;
-    await _flutterTts.speak(textToRead);
+    _pageController.text = (_currentPage + 1).toString();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: _isSearching
-            ? TextField(
-                controller: _searchController,
-                onChanged: _searchText,
-                decoration: InputDecoration(
-                  hintText: 'Search...',
-                  border: InputBorder.none,
-                ),
-              )
-            : Text(widget.book[0]),
-        actions: [
-          _isSearching
-              ? IconButton(
-                  icon: Icon(Icons.clear),
-                  onPressed: () {
-                    setState(() {
-                      _isSearching = false;
-                    });
-                  },
-                )
-              : IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {
-                    setState(() {
-                      _isSearching = true;
-                    });
-                  },
-                ),
-        ],
-      ),
+          title: Text(widget.book[0]),
+          backgroundColor: const Color.fromARGB(255, 176, 202, 248)),
       body: Stack(
         children: [
           PDFView(
             filePath: widget.book[1],
             enableSwipe: true,
+            autoSpacing: true,
+            pageFling: true,
+            onViewCreated: (controller) {
+              _pdfViewController = controller;
+            },
             onPageChanged: (page, total) {
               setState(() {
                 _currentPage = page!;
                 _totalPages = total!;
+                _pageController.text = (_currentPage + 1).toString();
               });
             },
           ),
           Positioned(
             bottom: 16.0,
-            right: 160.0,
+            right: 155.0,
             child: Container(
               padding: const EdgeInsets.all(8.0),
               decoration: BoxDecoration(
                 color: Colors.black.withOpacity(0.6),
                 borderRadius: BorderRadius.circular(8.0),
               ),
-              child: Text(
-                'Page ${_currentPage + 1} / $_totalPages',
-                style: const TextStyle(color: Colors.white),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Page: ",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  Container(
+                    width: 40.0, // Width of the text box
+                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: TextField(
+                      controller: _pageController,
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(color: Colors.white),
+                      decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        isDense: true,
+                      ),
+                      onChanged: (value) {
+                        int? pageNumber = int.tryParse(value);
+                        print('Page number: $pageNumber');
+                        if (pageNumber != null &&
+                            pageNumber > 0 &&
+                            pageNumber <= _totalPages) {
+                          setState(() {
+                            _currentPage = pageNumber - 1;
+                          });
+                          Future.delayed(Duration(milliseconds: 200), () {
+                            _pdfViewController?.setPage(_currentPage);
+                          });
+                          print('Current page: $_currentPage');
+                        }
+                      },
+                    ),
+                  ),
+                  Text(
+                    ' / $_totalPages',
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ],
               ),
             ),
           ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 176, 202, 248),
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.speed), label: 'Speed'),
           BottomNavigationBarItem(
@@ -147,7 +124,7 @@ class _BookDisplayState extends State<BookDisplay> {
             setState(() {
               isPlaying = !isPlaying;
             });
-            _startTTS(); // Start reading aloud when the play button is clicked
+            // _startTTS(); // Start reading aloud when the play button is clicked
           }
         },
       ),
